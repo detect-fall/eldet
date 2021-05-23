9a8feef37474b13fefd886196d124bb67842ecbd
G Y <Bhushan.GY@ellucian.com>
2021-05-23 23:35:07 +0530

    firebase added

--- fall_assist/lib/screens/fall_history.dart
+++ fall_assist/lib/screens/fall_history.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class FallHistory extends StatefulWidget {
   @override
@@ -8,8 +9,13 @@ class FallHistory extends StatefulWidget {
 }
 
 class _FallHistoryState extends State<FallHistory> {
+  //final fb = FirebaseDatabase.instance;
+
+  var recentJobsRef = FirebaseDatabase.instance.reference().child('sensorData');
+
   @override
   Widget build(BuildContext context) {
+    //final ref = fb.reference();
     SystemChrome.setSystemUIOverlayStyle(
         SystemUiOverlayStyle(statusBarColor: Colors.indigo));
     return SafeArea(
@@ -33,7 +39,41 @@ class _FallHistoryState extends State<FallHistory> {
             ),
           ),
         ),
-        body: Container(),
+        body: StreamBuilder(
+          stream: recentJobsRef.onValue,
+          builder: (context, snap) {
+            if (snap.hasData &&
+                !snap.hasError &&
+                snap.data.snapshot.value != null) {
+//taking the data snapshot.
+              DataSnapshot snapshot = snap.data.snapshot;
+              List item = [];
+              List _list = [];
+//it gives all the documents in this list.
+              _list = snapshot.value;
+//Now we're just checking if document is not null then add it to another list called "item".
+//I faced this problem it works fine without null check until you remove a document and then your stream reads data including the removed one with a null value(if you have some better approach let me know).
+              _list.forEach((f) {
+                if (f != null) {
+                  item.add(f);
+                }
+              });
+              return snap.data.snapshot.value == null
+//return sizedbox if there's nothing in database.
+                  ? SizedBox()
+//otherwise return a list of widgets.
+                  : ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: item.length,
+                      itemBuilder: (context, index) {
+                        return Text(item[index]['angle']);
+                      },
+                    );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
     );
   }
